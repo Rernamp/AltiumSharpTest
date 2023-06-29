2faf8ea88558fe0aa3008b646186d86a465cc9e2
rernamp <renamp100@gmail.com>
2023-06-29 21:37:01 +0300

    initial commit

---
+++ Application.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OriginalCircuit.AltiumSharp;
+
+namespace AltiumSharpTest {
+    public class Application {
+        public Application(string pathToSchDoc) {
+            PathToSchDoc = pathToSchDoc;
+        }
+
+        public void run() {
+            SchDocReader schDocReader = new SchDocReader();
+
+            var schDoc = schDocReader.Read(PathToSchDoc);
+        }
+
+        public readonly string PathToSchDoc;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using AltiumSharpTest;
+
+namespace AltiumScharpTest {
+    class Program {
+        public static void Main(string[] args) {
+            if (args.Count() != 1) {
+                return;
+            }
+            Application app = new Application(args[0]);
+
+            app.run();
+        }
+    }
+}
